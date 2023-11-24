62e6ca7b844a2efca958c04d735aaa8ccf0c8dd4
Trần Ngọc Hằng <139904578+usagichan131@users.noreply.github.com>
2023-11-24 19:03:43 +0700

    Update Start.py

--- Trần hằng/Start.py
+++ Trần hằng/Start.py
@@ -6,13 +6,22 @@ import sys
 import math
 import Board
 from Button import Button
-import settingmenu
-
-
-board_size = 15
 
 pygame.init()
 
+def get_font(size):
+    return pygame.font.Font("font.ttf", size)
+
+board_size = 15
+pygame.mixer.pre_init(44100, -16, 2, 2048)
+pygame.mixer_music.load('main_menu_music.ogg')
+pygame.mixer_music.play(-1)
+pygame.mixer_music.set_volume(0.5)
+sound_click=pygame.mixer.Sound("click_sound.ogg")
+music_playing = True
+sound_playing = True
+last_sound_play_time = pygame.time.get_ticks()
+pygame.init()
 
 def main():
     # game variables
@@ -40,20 +49,42 @@ def main():
     # text message
     label_1 = my_font.render('Black wins!', True, Board.WHITE, Board.BLACK)
     label_2 = my_font.render('White wins!', True, Board.WHITE, Board.BLACK)
-
+    music_playing = True
+    sound_playing = True
     # display the screen
     Board.draw_board(SCREEN, board_size)
     setting_button_image = pygame.image.load("cog_7712512.png")
     setting_button_image = pygame.transform.scale(setting_button_image,(50,50))
+    # music sound
+    music_icon_playing = pygame.image.load('music_on.png')
+    music_icon_playing = pygame.transform.scale(music_icon_playing,(50,50))
+    music_icon_paused = pygame.image.load('mute_music.png')
+    music_icon_paused = pygame.transform.scale(music_icon_paused,(50,50))
+    music_button_icon = music_icon_playing if music_playing else music_icon_paused
+    music_button = Button(image=music_button_icon, pos=(900,100),
+                        text_input=None, font=None, base_color="White",
+                        hovering_color="Pink") 
+    sound_icon_playing = pygame.image.load('sound_on.png')
+    sound_icon_playing = pygame.transform.scale(sound_icon_playing,(50,50))
+    sound_icon_paused = pygame.image.load('mute_sound.png')
+    sound_icon_paused = pygame.transform.scale(sound_icon_paused,(50,50))
+    sound_button_icon = sound_icon_playing if sound_playing else sound_icon_paused
+    sound_button = Button(image=sound_button_icon, pos=(900,150),
+                        text_input=None, font=None, base_color="White",
+                        hovering_color="Pink") 
     # Setting button
     setting_button = Button((setting_button_image),pos=(900,50), 
                             text_input=None, font=None, base_color="White", 
                             hovering_color="Pink")
+    # for button in [setting_button, music_button, sound_button]:
+    #         button.changeColor(mouse_pos)
+    #         button.update(screen)
     settings_menu_visible = False
     last_click_time = pygame.time.get_ticks()
-
+    
     # game loop
     while not game_over:
+        
         mouse_pos = pygame.mouse.get_pos()
         current_time = pygame.time.get_ticks()
         for event in pygame.event.get():
@@ -70,7 +101,22 @@ def main():
                             pygame.time.wait(200)
                               # Optional delay to avoid triggering both hide and show actions rapidly
                     last_click_time = current_time
-
+                if sound_button.checkForInput(mouse_pos):
+                    if sound_playing:
+                        sound_click.stop()
+                        sound_button.image = sound_icon_paused
+                    else:
+                        sound_click.play()
+                        sound_button.image = sound_icon_playing
+                    sound_playing = not sound_playing
+                if music_button.checkForInput(mouse_pos):
+                    if music_playing:
+                        pygame.mixer_music.stop()
+                        music_button.image = music_icon_paused
+                    else:
+                        pygame.mixer_music.play(-1)
+                        music_button.image = music_icon_playing
+                    music_playing = not music_playing
                 x_pos =event.pos[0]
                 y_pos = event.pos[1]
                 offset_x = (SCREEN.get_width() - Board.S_WIDTH) // 2
@@ -96,6 +142,8 @@ def main():
                     if Board.is_valid_loc(board, row, col):
                         Board.drop_piece(board, row, col, piece_1)
                         Board.draw_piece(SCREEN, board, board_size)
+                        if sound_playing:
+                            sound_click.play()
 
                     if Board.who_wins(board, piece_1, board_size):
                         print('Black wins!')
@@ -124,8 +172,9 @@ def main():
 
                 if game_over:
                         pygame.time.wait(4000)
-        setting_button.changeColor(mouse_pos)
-        setting_button.update(SCREEN)
+        for button in [setting_button, music_button, sound_button]:
+            button.changeColor(mouse_pos)
+            button.update(SCREEN)
 
         pygame.display.update()
         frames_per_sec.tick(FPS)
@@ -150,14 +199,14 @@ def show_settings_menu(screen):
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.MOUSEBUTTONDOWN:
-                if option1_button.checkForInput(mouse_pos):
+                if quit_button.checkForInput(mouse_pos):
                     pygame.quit()   
-                elif option2_button.checkForInput(mouse_pos):
+                elif restart_button.checkForInput(mouse_pos):
                     main()
                 elif back_button.checkForInput(mouse_pos):
                     pass
 
-        for button in [option1_button, option2_button, back_button]:
+        for button in [quit_button, restart_button, back_button]:
             button.changeColor(mouse_pos)
             button.update(screen)
 
