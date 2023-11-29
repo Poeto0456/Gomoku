fb378fe3463ada699ef0429a085f4273b8dfa456
Andrew Le <147686766+Poeto0456@users.noreply.github.com>
2023-11-29 16:03:51 +0700

    Add files via upload

---
+++ MonteCarloTreeNode.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+
+class TreeNode(object):
+
+    def __init__(self, prior_prob, parent=None):
+        self.parent = parent
+        self.children = {}  
+        self.reward = 0  
+        self.visited_times = 0
+        self.prior_prob = prior_prob  
+        
+    def is_root(self):
+        return self.parent is None
+
+    def expand(self, action, probability):
+        """
+        Nếu như cái action đã có tức child node đã được expand thì trả về child node luôn còn k thì tạo child_node là 1 cái
+        node mới rồi add nó vào dict
+        """
+        # 如果已经扩展过了（一般不可能）。 If it has been extended (generally impossible).
+        if action in self.children:
+            return self.children[action]
+
+        child_node = TreeNode(prior_prob=probability, parent = self)
+        self.children[action] = child_node
+
+        return child_node
+
+    def UCT_function(self, c=5.0):
+        greedy = c * self.prior_prob * np.sqrt(self.parent.visited_times) / (1 + self.visited_times)
+        if self.visited_times == 0:
+            return greedy
+        return self.reward / self.visited_times + greedy
+
+    def choose_best_child(self, c=5.0):
+        """
+        Kết quả trả ra sẽ giống như <(action(x_axis, y_axis), TreeNode)>, child_node[1] sẽ lấy ra TreeNode sau đó tính UCT_function(c) rồi mới trả ra kết quả max 
+        """
+        return max(self.children.items(), key=lambda child_node: child_node[1].UCT_function(c))
+
+    def backpropagate(self, value):
+        """
+        Nó sẽ tự update reward và visited times của cái node này trước
+        Sau đó check nó có phải root không, nếu k thì đệ quy lên parent
+        Mỗi lần đệ quy lên nó sẽ là lượt chơi của người khác nhau nên phải thêm dấu trừ
+        """
+        self.visited_times += 1
+        self.reward += value
+
+        if not self.is_root():
+            self.parent.backpropagate(-value)
